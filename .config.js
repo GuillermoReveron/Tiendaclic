c9af90d8481e588debe82328689b3695d7c6a3ee
Guillermo_Reveron <48895169+GuillermoReveron@users.noreply.github.com>
2026-05-29 14:45:06 -0300

    actualizacion 5

--- .config.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyD97Gu40eXRwsn28h1CmBwiCSAhROz9Ar4",
-  authDomain: "mitiendaia-produccion.firebaseapp.com",
-  projectId: "mitiendaia-produccion",
-  storageBucket: "mitiendaia-produccion.firebasestorage.app",
-  messagingSenderId: "1094943351799",
-  appId: "1:1094943351799:web:8ad38bfd16279ae705c4f2"
-};
